a82decfba8c22f2e36e790f8e8a1898c9f46bfb3
XunTongYao <dsyaohongwei@gmail.com>
2024-06-12 04:46:37 +0800

    test(test): 修改测试样例和文档

--- Readme.md
+++ Readme.md
@@ -1,5 +1,59 @@
 # XT_GUI 库
 
-这是一个基于 micropython 的 轻量级 GUI 库
+这是一个基于 micropython 的 轻量级 GUI 库.
+
+使用 micropython 中的 framebuf 库，构建一个高效帧缓存，GUI 所有的绘制操作都先在帧缓存上进行再同步到 GDDRAM，速度较快。
+
+注意：目前只支持 RGB565 屏幕
+
+## 硬件要求
+
+运行 python 解释器后剩余空闲内存大于整个屏幕帧缓存占用的空间，并至少留有 10KByte。
+
+```python
+# 占用空间简单计算公式
+_1位单色屏 = ceil(width*height/8)
+_RGB565屏 = ceil(width*height*2)
+```
+
+例如：
+
+SSD1306 分辨率为 128x64，色彩模式 1 位单色。帧缓存占用空间为 1024 Byte；
+
+ST7789 分辨率位 240x240，色彩模式 RGB565。帧缓存占用空间为 115,200 Byte
+
+## 快速上手
+
+### 远程调试(无需安装)
+
+该方法不会改变主机的文件系统，但运行速度非常慢，而且内存占用大。
+
+1. 准备运行`micropython`的开发板和一个`ST7789`的`LCD`屏幕，并使用 4 线串行接口的方式完成连接
+2. 克隆或下载本仓库到 PC 机本地
+3. 打开`gui_test.py`并修改相关引脚配置
+
+   ```python
+   # LED灯，进行PWM输出
+   LED = Pin(4, Pin.OUT)
+   # 请根据自身硬件情况更改引脚和上拉电阻设置
+   # 退出按键
+   BTN_ESCAPE = Pin(5, Pin.IN)
+   # 确认/进入按键
+   BTN_ENTER = Pin(6, Pin.IN, Pin.PULL_UP)
+   # 下键
+   BTN_DOWN = Pin(7, Pin.IN, Pin.PULL_UP)
+   # 上键
+   BTN_UP = Pin(8, Pin.IN, Pin.PULL_UP)
+
+   # 请根据自身硬件情况更改引脚和波特率
+   # 初始化显示屏
+   spi0 = SPI(0, baudrate=30_000_000, phase=1, polarity=1, sck=Pin(2), mosi=Pin(3))
+   display = st7789.ST7789(spi0, 240, 240, reset=Pin(0, Pin.OUT), dc=Pin(1, Pin.OUT))
+   displayer = st7789.ST7789_API(display)
+   ```
+
+4. 下载官方的 [mpremote](https://docs.micropython.org/en/latest/reference/mpremote.html#mpremote) 工具`pip3 install mpremote`
+5. 挂载代码目录到主机(请确保运行命令时路径处于代码目录，串口不被其他程序占用)`mpremote mount .`
+6. 运行简单演示`>>> import gui_test.py`
 
 ## 特性
--- gui_test.py
+++ gui_test.py
@@ -1,52 +1,45 @@
-from driver import fj08kn, st7789
-from gui.widgets.containers import XListView
-from machine import Pin, SPI, ADC, PWM
+from machine import Pin, SPI, PWM
+from driver import st7789
 from gui import ufont
-from gui.xt_gui import *
-from gui.widgets.buttons import *
-from gui.widgets.inputs import *
-from gui.key_proxy import KeyProxy
-import gui.xt_gui as XTGUI
+from gui.utils import *
+from gui.xt_gui import XT_GUI
+from gui.widgets.buttons import XButton, XCheckbox, XRadio
+from gui.widgets.inputs import XSlider
+from gui.widgets.containers import XListView
+from gui.key_proxy import KeyProxySimplified
 
+# LED灯，进行PWM输出
 LED = Pin(4, Pin.OUT)
-BTN_B = Pin(5, Pin.IN)
-BTN_A = Pin(6, Pin.IN, Pin.PULL_UP)
-BTN_START = Pin(7, Pin.IN, Pin.PULL_UP)
-BTN_SELECT = Pin(8, Pin.IN, Pin.PULL_UP)
+# 请根据自身硬件情况更改引脚和上拉电阻设置
+# 退出按键
+BTN_ESCAPE = Pin(5, Pin.IN)
+# 确认/进入按键
+BTN_ENTER = Pin(6, Pin.IN, Pin.PULL_UP)
+# 下键
+BTN_DOWN = Pin(7, Pin.IN, Pin.PULL_UP)
+# 上键
+BTN_UP = Pin(8, Pin.IN, Pin.PULL_UP)
 
-XTGUI.DEBUG = False
-# machine.freq(250000000)
-
-
+# 请根据自身硬件情况更改引脚和波特率
+# 初始化显示屏
 spi0 = SPI(0, baudrate=30_000_000, phase=1, polarity=1, sck=Pin(2), mosi=Pin(3))
 display = st7789.ST7789(spi0, 240, 240, reset=Pin(0, Pin.OUT), dc=Pin(1, Pin.OUT))
 displayer = st7789.ST7789_API(display)
 
-Joystick = fj08kn.FJ08K(ADC(Pin(29)), ADC(Pin(28)))
-Up = Joystick.get_simulate_key(1)
-Down = Joystick.get_simulate_key(3)
 
-
+# 下面的代码不需要额外更改
+# 创建XT_GUI实例
 GUI = XT_GUI(
     displayer,
-    ufont.BMFont("/Resource/Fonts/16x16Sim.bmf"),
-    cursor_img_file="/Resource/Img/Cursor21x32.pbm",
+    ufont.BMFont("./resource/fonts/16x16Sim.bmf", True),
+    cursor_img_file="./resource/Img/Cursor21x32.pbm",
     loop_focus=True,
 )
 
 
-# GUI.DrawBinaryImage("/Resource/Img/Cursor21x32.pbm", (50, 50), XT_GUI.WHITE)
-def printHello(Key_ID):
+# 声明回调函数，用于按钮等的触发
+def printHello(_):
     print("Hello")
-
-
-B = XButton((0, 0), (32 + 6, 16 + 6), context="你好", key_input=printHello)
-KL = XButton((32 + 7, 0), (32 + 6, 16 + 6), context="世界")
-Checkbox = XCheckbox((32 + 7 + 32 + 7, 0), 32)
-Radio = XRadio((32 + 7 + 32 + 7 + 33, 0), 32)
-slider = XSlider(
-    (0, 40), (50, 24), 0, 100, color=GREEN, show_text=True, orientation=XSlider.VERTICAL
-)
 
 
 def slider_plus_10(_):
@@ -57,12 +50,26 @@ def slider_mius_10(_):
     slider.set_value(slider.value - 10)
 
 
+# 设置了按下触发的回调，焦点选中并按下确认/进入按键会打印"Hello"
+hello_button = XButton((0, 0), (32 + 6, 16 + 6), context="你好", key_input=printHello)
+# 没有设置回调，按下确认按键什么都不会发生
+world_b = XButton((32 + 7, 0), (32 + 6, 16 + 6), context="世界")
+# 多选框演示
+checkbox = XCheckbox((32 + 7 + 32 + 7, 0), 32)
+# 单选框演示，勾选后PWM输出将启用
+radio = XRadio((32 + 7 + 32 + 7 + 33, 0), 32)
+# 滑动条，焦点选中并按下确认按键会进入到该控件，按上下键调节PWM输出占空比
+slider = XSlider(
+    (0, 40), (50, 24), 0, 100, color=GREEN, show_text=True, orientation=XSlider.VERTICAL
+)
+# 纵向列表视图演示，这是一个容器，可以添加控件到其中
 listv = XListView((0, 70), (100, 100))
 
-
-for w in [B, KL, Checkbox, Radio, slider, listv]:
+# 向XT_GUI实例添加上面的控件
+for w in [hello_button, world_b, checkbox, radio, slider, listv]:
     GUI.add_widget(w)
 
+# 向列表视图加入两个按钮，快速调节滑动条的值
 listv.add_widget(
     XButton((0, 0), (32 + 6, 16 + 6), context="+10", key_input=slider_plus_10)
 )
@@ -70,21 +77,19 @@ listv.add_widget(
     XButton((0, 16 + 6), (32 + 6, 16 + 6), context="-10", key_input=slider_mius_10)
 )
 
-
-KeyP = KeyProxy([BTN_B, BTN_A, Down, Up, BTN_START])
-KeyP.ReleaseArgs[0:4] = KEY_ESCAPE, KEY_MOUSE0, KEY_DOWN, KEY_UP, KEY_ESCAPE
-KeyP.HoldArgs[0:4] = KEY_ESCAPE, KEY_MOUSE0, KEY_DOWN, KEY_UP, KEY_ESCAPE
-KeyP.ReleaseCALLBACKs[0:4] = [GUI.key_response] * 5
-KeyP.HoldCALLBACKs[0:4] = [GUI.key_response] * 5
+# 按键消抖相关
+key_p = KeyProxySimplified([BTN_ESCAPE, BTN_ENTER, BTN_DOWN, BTN_UP])
+key_p.press_callbacks[0:3] = [GUI.key_response] * 4
+key_p.press_args[0:3] = KEY_ESCAPE, KEY_MOUSE0, KEY_DOWN, KEY_UP
 
 pwm = PWM(LED)
 pwm.freq(1000)
 
 
 while True:
-    KeyP.Scan()
+    key_p.scan()
     GUI.show_gui()
-    if Radio.checked:
+    if radio.checked:
         pwm.duty_u16(int(slider.get_percent() * 65535))
     else:
         pwm.duty_u16(0)
